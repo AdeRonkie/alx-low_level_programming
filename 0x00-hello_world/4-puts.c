2bb10d1ebe3f9a4e04f3cd7ec15ec0e993c186a2
AdeRonkie <adekanmbichristiana@gmail.com>
2022-10-13 16:55:36 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- *Return: Always 0 (Success)
- */
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+#include <stdio.h>                                                                                        
+/**                                                                                                       
+ *main-entry point for the program.                                                                       
+ *                                                                                                        
+ *Return:0 if no errors, return non zero if errors                                                        
+ */                                                                                                       
+int main(void)                                                                                            
+{                                                                                                         
+	                printf("with proper grammar, but the outcome is a piece of art,\n");                      
+			                        return (0);                                                                       
 }
