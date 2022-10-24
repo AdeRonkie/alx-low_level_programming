4ec354c573935c8ffb9b99766f3a397662010266
AdeRonkie <adekanmbichristiana@gmail.com>
2022-10-24 15:44:03 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,19 +4,7 @@
 *@m:Variable to be reset.
 *Return:Void.
 */
-
 void reset_to_98(int *n)
 {
-m = 402;
-}
-/**
-*main - parameters are passed by value
-*Return: Always 0.
-*/
-int *n(void)
-{
-int *n;
 n = 98;
-reset_to_98(n);
-return (0);
 }
