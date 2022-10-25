4dd4561b77f50b1b60f6e3710792835c3be842a8
AdeRonkie <adekanmbichristiana@gmail.com>
2022-10-25 10:28:54 -0700

    Print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,10 @@ int count = 0;
 while (count >= 0)
 {
 if (s[count] == '\0')
-break;										count++;									}
+break;
+count++;
+}
+
 for (count--; count >= 0; count--)
 _putchar(s[count]);
 _putchar('\n');
