876c6106cbade2c8cae64c2c5066601e42dae170
AdeRonkie <adekanmbichristiana@gmail.com>
2022-10-27 12:38:47 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/** 
+/**
 *rot13 - encodes a string using rot13 
 *@s: input string. 
-*Return: the pointer to dest. 
+*Return: the pointer to dest.
 */
 
 char *rot13(char *s)
@@ -10,10 +10,10 @@ char *rot13(char *s)
 int i, j;
 char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	
+
 for (i = 0; s[i] != '\0'; i++)
 {
-	
+
 for (j = 0; x[j] != '\0'; j++)
 {
 if (s[i] == x[j])
